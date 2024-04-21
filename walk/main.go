2e1b834855887aca155f144e3f1b5ca61d322b05
ccchrunik <yingchiao.chen@gmail.com>
2024-04-21 16:53:41 -0700

    [walk] add archiving option

--- walk/main.go
+++ walk/main.go
@@ -21,12 +21,16 @@ type config struct {
 	del bool
 	// log destination writer
 	wLog io.Writer
+	// archive directory
+	archive string
 }
 
 func main() {
 	root := flag.String("root", ".", "Root directory to start")
 	logFile := flag.String("log", "", "Log deletes to this file")
+	// Action options
 	list := flag.Bool("list", false, "List files only")
+	archive := flag.String("archive", "", "Archive directory")
 	ext := flag.String("ext", "", "File extension to filter out")
 	size := flag.Int64("size", 0, "Minimum file size")
 	del := flag.Bool("del", false, "Delete files")
@@ -47,11 +51,12 @@ func main() {
 	}
 
 	c := config{
-		ext:  *ext,
-		size: *size,
-		list: *list,
-		del:  *del,
-		wLog: f,
+		ext:     *ext,
+		size:    *size,
+		list:    *list,
+		del:     *del,
+		wLog:    f,
+		archive: *archive,
 	}
 
 	if err := run(*root, os.Stdout, c); err != nil {
@@ -76,6 +81,14 @@ func run(root string, out io.Writer, cfg config) error {
 			return listFile(path, out)
 		}
 
+		// Archive files and continue if successful
+		if cfg.archive != "" {
+			if err := archiveFile(cfg.archive, root, path); err != nil {
+				return err
+			}
+		}
+
+		// Delete files
 		if cfg.del {
 			return deleteFile(path, delLogger)
 		}
