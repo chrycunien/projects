cbc9ebb8dc53d1c6df4b45436fa4bfae2056075f
ccchrunik <yingchiao.chen@gmail.com>
2024-04-21 16:30:27 -0700

    [walk] add logging for delete operations

--- walk/main.go
+++ walk/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"io/fs"
+	"log"
 	"os"
 	"path/filepath"
 )
@@ -18,21 +19,39 @@ type config struct {
 	list bool
 	// delete files
 	del bool
+	// log destination writer
+	wLog io.Writer
 }
 
 func main() {
 	root := flag.String("root", ".", "Root directory to start")
+	logFile := flag.String("log", "", "Log deletes to this file")
 	list := flag.Bool("list", false, "List files only")
 	ext := flag.String("ext", "", "File extension to filter out")
 	size := flag.Int64("size", 0, "Minimum file size")
 	del := flag.Bool("del", false, "Delete files")
 	flag.Parse()
 
+	var (
+		f   = os.Stdout
+		err error
+	)
+
+	if *logFile != "" {
+		f, err = os.OpenFile(*logFile, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0644)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		defer f.Close()
+	}
+
 	c := config{
 		ext:  *ext,
 		size: *size,
 		list: *list,
 		del:  *del,
+		wLog: f,
 	}
 
 	if err := run(*root, os.Stdout, c); err != nil {
@@ -42,6 +61,8 @@ func main() {
 }
 
 func run(root string, out io.Writer, cfg config) error {
+	delLogger := log.New(cfg.wLog, "DELETE FILE: ", log.LstdFlags)
+
 	return filepath.Walk(root, func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
 			return err
@@ -56,7 +77,7 @@ func run(root string, out io.Writer, cfg config) error {
 		}
 
 		if cfg.del {
-			return deleteFile(path)
+			return deleteFile(path, delLogger)
 		}
 
 		return listFile(path, out)
